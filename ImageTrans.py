c4a1bc9df06c7e103bd249009fe91982a4f76367
Ivan Guerra <ivan.aguerra616@gmail.com>
2013-02-07 00:24:42 -0600

    subo la actualizacion con commit

--- ImageTrans.py
+++ ImageTrans.py
@@ -1,3 +1,4 @@
+ 
 import PIL 
 import Tkinter
 from Tkinter import *
@@ -51,25 +52,89 @@ def convertBin(image=imageArgu):
     imagen.show()
 
 def vecinity(image=imageArgu):
+    
+ 
     imagen=Image.open(image)
     pixels=imagen.load()
     width,height=imagen.size
-    for i in range(width):
-        for j in range(height):
-            if(i==0 and j==0):
+    howmany=raw_input("cuantos filtros quieres?: ")
+    filtros=int(howmany)
+    for w in range(filtros):
+        for i in range(width):
+            for j in range(height):
                 r,g,b=pixels[i,j]
-                r1,g1,b1=pixels[i+1,j]
-                r2,g2,b2=pixels[i,j+1]
-                newPix=(((r+r1+r2)+(g+g1+g2)+(b+b1+b2))/3)
-                pixels[i,j]=newPix
-            elif(i==0 and j== height):
-                r,g,b=pixels[i,j]
-                r1,g1,b1=pixels[i+1,j]
-                r2,g2,b2=pixels[i,j+1]
-                r3,g3,b3=pixels[i-1,j]
-                r4,g4,b4=pixels[i,j-1]
-                newPix=(((r+r1+r2+r3+r4)+(g+g1+g2+g3+g4)+(b+b1+b2+b3+b4))/5)
+                gray=int((r+g+b)/3)
+            
 
+    
+ 
+
+                if(i==0 and j==0):
+                    r,g,b=(gray,gray,gray)
+                    r1,g1,b1=pixels[i+1,j]
+                    r2,g2,b2=pixels[i,j+1]
+                    newPix=(((r+r1+r2)+(g+g1+g2)+(b+b1+b2))/3)
+                    pixels[i,j]=(newPix,newPix,newPix)
+                elif(i==0 and j== height-1):
+                    r,g,b=(gray,gray,gray)
+                    r1,g1,b1=pixels[i+1,j]
+                    r2,g2,b2=pixels[i,j-1]       
+                    newPix=(((r+r1+r2)+(g+g1+g2)+(b+b1+b2))/3)
+                    pixels[i,j]=(newPix,newPix,newPix)
+                elif(i==width-1 and j == 0):
+                    r,g,b=(gray,gray,gray)
+                    r1,g1,b1=pixels[i-1,j]
+                    r2,g2,b2=pixels[i,j+1]
+                    newPix=(((r+r1+r2)+(g+g1+g2)+(b+b1+b2))/3)
+                    pixels[i,j]=(newPix,newPix,newPix)
+                elif(i==width-1 and j == height-1):
+                    r,g,b=(gray,gray,gray)
+                    r1,g1,b1=pixels[i-1,j]
+                    r2,g2,b2=pixels[i,j-1]
+                    newPix=(((r+r1+r2)+(g+g1+g2)+(b+b1+b2))/3)
+                    pixels[i,j]=(newPix,newPix,newPix)
+                elif(j==0 and i>=1 and i < width-1):
+                    r,g,b=(gray,gray,gray)
+                    r1,g1,b1=pixels[i-1,j]
+                    r2,g2,b2=pixels[i+1,j]
+                    r3,g3,b3=pixels[i,j+1]
+                    newPix=(((r+r1+r2+r3)+(g+g1+g2+g3)+(b+b1+b2+b3))/4)
+                    pixels[i,j]=(newPix,newPix,newPix)
+                elif(i==0 and j>=1 and j<height-1):
+                    r,g,b=(gray,gray,gray)
+                    r1,g1,b1=pixels[i,j-1]
+                    r2,g2,b2=pixels[i,j+1]
+                    r3,g3,b3=pixels[i+1,j]
+                    newPix=(((r+r1+r2+r3)+(g+g1+g2+g3)+(b+b1+b2+b3))/4)
+                    pixels[i,j]=(newPix,newPix,newPix)
+                elif(i==width-1 and j>=1 and j < height-1):
+                    r,g,b=(gray,gray,gray)
+                    r1,g1,b1=pixels[i,j-1]
+                    r2,g2,b2=pixels[i,j+1]
+                    r3,g3,b3=pixels[i-1,j]
+                    newPix=(((r+r1+r2+r3)+(g+g1+g2+g3)+(b+b1+b2+b3))/4)
+                    pixels[i,j]=(newPix,newPix,newPix)
+                elif(j==height-1 and i>=1 and i < width-1):
+                    r,g,b=(gray,gray,gray)
+                    r1,g1,b1=pixels[i+1,j]
+                    r2,g2,b2=pixels[i-1,j]
+                    r3,g3,b3=pixels[i,j-1]
+                    newPix=(((r+r1+r2+r3)+(g+g1+g2+g3)+(b+b1+b2+b3))/4)
+                    pixels[i,j]=(newPix,newPix,newPix)
+                else:
+                    r,g,b=(gray,gray,gray)
+                    r1,g1,b1=pixels[i+1,j]
+                    r2,g2,b2=pixels[i-1,j]
+                    r3,g3,b3=pixels[i,j-1]
+                    r4,g4,b4=pixels[i,j+1]
+                    newPix=(((r+g+b)/3)+((r1+g1+b1)/3)+((r2+g2+b2)/3)+((r3+g3+b3)/3)+((r4+g4+b4)/3))/5
+                    if(newPix>255):
+                        newPix=255
+                        
+                    pixels[i,j]=(newPix,newPix,newPix)
+                        
+ 
+ 
 
     imagen.show()
             
