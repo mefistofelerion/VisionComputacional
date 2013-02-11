1f69613851121ed7eca814ecf8e3e887575ccf09
Ivan Guerra <ivan.aguerra616@gmail.com>
2013-02-11 10:17:34 -0600

    unas actualizaciones pquenias

--- DeteccionBordes.py
+++ DeteccionBordes.py
@@ -27,90 +27,19 @@ def detectarBordes():
             r,g,b=pixels[i,j]#toma el valor del pixel actual
             gray=int((r+g+b)/3)#saca el promedio para la escala de grises
             pixels[i,j]=(gray,gray,gray)#lo reemplaza
-            ''' #despues pasara por varios filtros
-            for w in range(1):
-            for i in range(width):
-            for j in range(height):
+          
             r,g,b=pixels[i,j]
             gray=int((r+g+b)/3)#saca el promedio de grises
             
-            
-            #primero checa todas las esquinas
-            if(i==0 and j==0):
-            r,g,b=(gray,gray,gray)
-            r1,g1,b1=pixels[i+1,j]
-            r2,g2,b2=pixels[i,j+1]
-            newPix=(((r+r1+r2)+(g+g1+g2)+(b+b1+b2))/3)
-            pixels[i,j]=(newPix,newPix,newPix)
-            elif(i==0 and j== height-1):
-            r,g,b=(gray,gray,gray)
-            r1,g1,b1=pixels[i+1,j]
-            r2,g2,b2=pixels[i,j-1]       
-            newPix=(((r+r1+r2)+(g+g1+g2)+(b+b1+b2))/3)
-            pixels[i,j]=(newPix,newPix,newPix)
-            elif(i==width-1 and j == 0):
-            r,g,b=(gray,gray,gray)
-            r1,g1,b1=pixels[i-1,j]
-            r2,g2,b2=pixels[i,j+1]
-            newPix=(((r+r1+r2)+(g+g1+g2)+(b+b1+b2))/3)
-            pixels[i,j]=(newPix,newPix,newPix)
-            elif(i==width-1 and j == height-1):
-            r,g,b=(gray,gray,gray)
-            r1,g1,b1=pixels[i-1,j]
-            r2,g2,b2=pixels[i,j-1]
-            newPix=(((r+r1+r2)+(g+g1+g2)+(b+b1+b2))/3)
-            pixels[i,j]=(newPix,newPix,newPix)
-            #despues checa todos los bordes
-            elif(j==0 and i>=1 and i < width-1):
-            r,g,b=(gray,gray,gray)
-            r1,g1,b1=pixels[i-1,j]
-            r2,g2,b2=pixels[i+1,j]
-            r3,g3,b3=pixels[i,j+1]
-            newPix=(((r+r1+r2+r3)+(g+g1+g2+g3)+(b+b1+b2+b3))/4)
-            pixels[i,j]=(newPix,newPix,newPix)
-            elif(i==0 and j>=1 and j<height-1):
-            r,g,b=(gray,gray,gray)
-            r1,g1,b1=pixels[i,j-1]
-            r2,g2,b2=pixels[i,j+1]
-            r3,g3,b3=pixels[i+1,j]
-            newPix=(((r+r1+r2+r3)+(g+g1+g2+g3)+(b+b1+b2+b3))/4)
-            pixels[i,j]=(newPix,newPix,newPix)
-            elif(i==width-1 and j>=1 and j < height-1):
-            r,g,b=(gray,gray,gray)
-            r1,g1,b1=pixels[i,j-1]
-            r2,g2,b2=pixels[i,j+1]
-            r3,g3,b3=pixels[i-1,j]
-            newPix=(((r+r1+r2+r3)+(g+g1+g2+g3)+(b+b1+b2+b3))/4)
-            pixels[i,j]=(newPix,newPix,newPix)
-            elif(j==height-1 and i>=1 and i < width-1):
-            r,g,b=(gray,gray,gray)
-            r1,g1,b1=pixels[i+1,j]
-            r2,g2,b2=pixels[i-1,j]
-            r3,g3,b3=pixels[i,j-1]
-            newPix=(((r+r1+r2+r3)+(g+g1+g2+g3)+(b+b1+b2+b3))/4)
-            pixels[i,j]=(newPix,newPix,newPix)
-            #todo el resto de los pixeles 
-            else:
-            r,g,b=(gray,gray,gray)#toma el valor d grises del pixel actual
-            r1,g1,b1=pixels[i+1,j]#toma cada uno de los pixeles vecinos
-            r2,g2,b2=pixels[i-1,j]#y saca el promedio
-            r3,g3,b3=pixels[i,j-1]
-            r4,g4,b4=pixels[i,j+1]
-            newPix=(((r+g+b)/3)+((r1+g1+b1)/3)+((r2+g2+b2)/3)+((r3+g3+b3)/3)+((r4+g4+b4)/3))/5#promediado del nuevo pixel
-            if(newPix>255):#solo para asegurar que el pixel no se pase del RGB
-            newPix=255
-                        
-            pixels[i,j]=(newPix,newPix,newPix)#se reemplaza'''
-
-    aplicarMascara(imageLoaded)
-
+    imagenConMascara=aplicarMascara(imageLoaded)
+    imagenConMascara.show()
 
 def aplicarMascara(imagen):
     #mascaraX= [[0,1,0],[1,-4,1],[0,1,0]]
     mascaraX= [[-1,0,1], [-2,0,2],[-1,0,1]]
     pixels=imagen.load()
-    im=Image.open(original)
-    pix=im.load()
+    nuevaImagen=Image.open(original)
+    pix=nuevaImagen.load()
     wid,hei=imagen.size 
     for i in range(wid):
         for j in range(hei):
@@ -132,7 +61,7 @@ def aplicarMascara(imagen):
             else:
                 None
                 
-    im.show()
+    return nuevaImagen
     
 def main():
     load()
